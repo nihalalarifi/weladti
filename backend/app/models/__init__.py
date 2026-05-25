762af7212fb81fb53a49f9ad880d01841958772c
nihalalarifi <nihal.a.alarifi@gmail.com>
2026-05-25 05:45:34 +0300

    Add backend/app/models/__init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .health_record import HealthRecord
+from .prediction import Prediction
+from .chat_message import ChatMessage
+
+__all__ = ["User", "HealthRecord", "Prediction", "ChatMessage"]
